a86a969169c0c4a7c12a37fcaa0a8df331769cbd
Lokendra0001 <rajpurohitlokendra62@gmail.com>
2026-06-05 18:55:00 +0530

    fix: incorporate turbopack root path restriction

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -11,6 +17,12 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
       },
     ],
+  },
+  experimental: {
+    turbopack: {
+      // यह Turbopack को आपके इसी client फ़ोल्डर के अंदर सीमित रखेगा
+      root: __dirname,
+    },
   },
 };
 
